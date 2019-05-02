89bac9999d441e23b5968caf8dfc4b354799e71e
CUAgainstKennedy <cu.against.kennedy.github@gmail.com>
2019-05-02 14:50:38 -0600

    peter first commit

--- peter/first_dive.R
+++ peter/first_dive.R
@@ -3,9 +3,14 @@ library(dplyr)
 library(ggplot2)
 library(RColorBrewer)
 
-main = read.csv('./police_stop_data_main_2018.csv')
-res = read.csv('./police_stop_data_results_2018.csv')
-desc = read.csv('./police_stop_data_dictionary.csv')
+DATA.DIR = '../data'
+
+main = read.csv(paste0(DATA.DIR,'/police_stop_data_main_2018.csv'))
+res = read.csv(paste0(DATA.DIR,'/police_stop_data_results_2018.csv'))
+desc = read.csv(paste0(DATA.DIR,'/police_stop_data_dictionary.csv'))
+
+names(main) = main %>% names %>% tolower
+names(res) = res %>% names %>% tolower
 
 tellme = function(x) {dict %>% filter(Column.Name==x) %>% select(Description) %>% return}
 
@@ -15,7 +20,7 @@ main$race2[ main$race2=='W' & main$ethnic=='H' ] = 'H'
 race.counts = table(main$race2) %>% as.data.frame %>% set_colnames(c('race2','freq'))
 main = main %>% left_join(race.counts,by='race2')
 
-p = ggplot(main %>% filter(Min<60), aes(x=race2,y=Min,fill=as.factor(freq))) +
+p = ggplot(main %>% filter(min<60), aes(x=race2,y=min,fill=as.factor(freq))) +
     geom_boxplot() +
     scale_fill_brewer(palette = 'RdYlGn')
                      
